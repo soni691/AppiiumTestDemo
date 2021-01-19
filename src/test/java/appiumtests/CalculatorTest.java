549ee38ccf907823329d817de0b7c417af80b7b6
soni691 <75196665+soni691@users.noreply.github.com>
2021-01-19 19:04:30 +0530

    appium web
    appium web

--- src/test/java/appiumtests/CalculatorTest.java
+++ src/test/java/appiumtests/CalculatorTest.java
@@ -4,11 +4,15 @@ import java.beans.DesignMode;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class CalculatorTest {
 	static //WebDriver driver;
@@ -16,7 +20,8 @@ public class CalculatorTest {
 
 	public static void main(String[] args) {
 		try {
-			openCalculator();
+			//openCalculator();
+			openMobileWebApp();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.getCause();
@@ -38,7 +43,26 @@ public class CalculatorTest {
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
 		driver = new AppiumDriver<MobileElement>(url,cap);
 		System.out.println("Application Started............");
+		WebElement two = driver.findElement(By.id("com.asus.calculator:id/digit2"));	
+		WebElement plus = driver.findElement(By.id("com.asus.calculator:id/plus"));
+		WebElement three = driver.findElement(By.id("com.asus.calculator:id/digit3"));
+		WebElement equals = driver.findElement(By.id("com.asus.calculator:id/equal"));
+		two.click();
+		plus.click();
+		three.click();
+		equals.click();	
+	}
+	public static void openMobileWebApp() throws MalformedURLException {
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "ASUS_ZENFONE_MAX_M2");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
+		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
 		
+		driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"),cap);
+		driver.get("https://www.flipkart.com/");
+		System.out.println("Application Started............");
+	
 	}
 
 }
